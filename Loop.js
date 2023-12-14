4f7c492248cec0325cc37dca1d2f9e27ad42a34e
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-12-14 10:48:41 +0500

    change

---
+++ Loop.js
@@ -0,0 +1,78 @@
+// 1----- For Loop
+
+// for (i = 1; i <= 5; i++) {
+//     console.log("Hello World");
+// }
+
+// for (count = 1; count <= 1000; count++) {
+//     console.log("Hello World");
+// }
+
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+
+// }
+
+// console.log("sum is = ", sum);
+// console.log("Loop is ended");
+
+// let sum = 0;
+// let n = 10;
+
+// for (let i = 1; i <= n; i++) {
+//     // console.log("i =", i);
+//     console.log("I Love You Kkkk Kiran");
+// }
+
+
+// 2----- While Loop
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log("hello world");
+//     i++;
+// }
+
+// 3----- do While Loop
+
+// let i = 1;
+// do {
+//     console.log("i=", i);
+//     i++;
+// } while (i <= 5);
+
+// 4----- For Of Loop
+
+// syntax Of For Of Loop
+
+/* 
+
+for ( let val of strVar) {
+     do some work
+ }
+ 
+ */
+
+// let str = "HelloWorld";
+// size = 0;
+
+// for (let i of str) {
+//     console.log("i=", i);
+//     size++;
+// }
+// console.log("size of str=", size);
+
+// 5----- For In Loop
+
+// let student = {
+//     name: "sham",
+//     age: 22,
+//     cgpa: 3.5,
+//     ispass: true,
+// };
+
+// for (let key in student) {
+//     console.log("key=", key, "& value=", student[key]);
+// }
---
+++ practise.js
@@ -0,0 +1,28 @@
+// print all even numbers from 0 to 100
+
+//Practise Q1
+
+// for (let num = 0; num <= 100; num++) {
+//     if (num % 2 === 0) {  // For print even numbers
+//         console.log("num =", num);
+
+/*    OR    */
+//     }
+//     if (num % 2 !== 0) {  // For print Odd numbers
+//         console.log("num =", num);
+//     }
+// }
+
+// console.log("Loop Ended"); 
+
+//Practise Q2
+
+// let GameNum = 20;
+// let UserNum = prompt("Guess the Game Number");
+
+// while (GameNum != UserNum) {
+//     UserNum = prompt("You entered the wrong number guess again");
+// }
+
+// console.log("Congratulations you entered the correct number");
+
